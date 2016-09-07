9145a5da9be9242741f6f678a1ce3f3889bac015
cbstodd <cbstodd@gmail.com>
2016-09-07 16:07:23 -0400

    added passport authentication and users

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express          = require('express'),
     bodyParser       = require('body-parser'),
     mongoose         = require('mongoose'),
     courseController = require('./server/controllers/CourseController'),
+    userController   = require('./server/controllers/UserController'),
+    passport         = require('passport'),
+    authController   = require('./server/controllers/auth'),
     app              = express();
 
 mongoose.connect('mongodb://localhost:27017/noobsee');
@@ -14,14 +17,20 @@ var router = express.Router();
 
 // Create endpoint handlers for /courses
 router.route('/courses')
-      .post(courseController.postCourses)
-      .get(courseController.getCourses);
+      .post(authController.isAuthenticated, courseController.postCourses)
+      .get(authController.isAuthenticated, courseController.getCourses);
 
 // Create endpoint handlers for /courses/:course_id
 router.route('/courses/:course_id')
-      .get(courseController.getCourse)
-      .put(courseController.putCourse)
-      .delete(courseController.deleteCourse);
+      .get(authController.isAuthenticated, courseController.getCourse)
+      .put(authController.isAuthenticated, courseController.putCourse)
+      .delete(authController.isAuthenticated, courseController.deleteCourse);
+
+// Create endpoint handlers for /users
+router.route('/users')
+      .post(userController.postUsers)
+      .get(authController.isAuthenticated, userController.getUsers);
+
 
 // Register all our routes with /api
 app.use('/api', router);
--- server/controllers/CourseController.js
+++ server/controllers/CourseController.js
@@ -1,7 +1,7 @@
 // Load required packages
 var Course = require('../models/course');
 
-// Create endpoint /api/courses for POSTS
+// Create endpoint /api/courses for POST
 exports.postCourses = function( req, res ) {
     // Create a new instance of the Course model
     var course = new Course();
@@ -13,6 +13,7 @@ exports.postCourses = function( req, res ) {
     course.language = req.body.language;
     course.description = req.body.description;
     course.title = req.body.title;
+    course.userId = req.user._id;
 
     // Save the course and check for errors
     course.save(function( err ) {
@@ -26,7 +27,7 @@ exports.postCourses = function( req, res ) {
 // Create endpoint /api/courses for GET
 exports.getCourses = function( req, res ) {
     // Use the Course model to find all course
-    Course.find(function( err, courses ) {
+    Course.find({ userId: req.user._id }, function( err, courses ) {
         if (err)
             res.send(err);
 
@@ -37,7 +38,7 @@ exports.getCourses = function( req, res ) {
 // Create endpoint /api/courses/:course_id for GET
 exports.getCourse = function( req, res ) {
     // Use the Course model to find a specific course
-    Course.findById(req.params.course_id, function( err, course ) {
+    Course.find({ userId: req.user._id, _id: req.params.course_id }, function( err, course ) {
         if (err)
             res.send(err);
 
@@ -48,32 +49,21 @@ exports.getCourse = function( req, res ) {
 // Create endpoint /api/courses/:course_id for PUT
 exports.putCourse = function( req, res ) {
     // Use the Course model to find a specific course
-    Course.findById(req.params.course_id, function( err, course ) {
+    Course.update({
+        userId: req.user._id,
+        _id:    req.params.course_id
+    }, { quantity: req.body.quantity }, function( err, num, raw ) {
         if (err)
             res.send(err);
 
-        // Update the existing course quantity
-        course.progress = req.body.progress;
-        course.imageUrl = req.body.imageUrl;
-        course.experienceLevel = req.body.experienceLevel;
-        course.language = req.body.language;
-        course.description = req.body.description;
-        course.title = req.body.title;
-
-        // Save the course and check for errors
-        course.save(function( err ) {
-            if (err)
-                res.send(err);
-
-            res.json(course);
-        });
+        res.json({ message: num + ' updated' });
     });
 };
 
 // Create endpoint /api/courses/:course_id for DELETE
 exports.deleteCourse = function( req, res ) {
     // Use the Course model to find a specific course and remove it
-    Course.findByIdAndRemove(req.params.course_id, function( err ) {
+    Course.remove({ userId: req.user._id, _id: req.params.course_id }, function( err ) {
         if (err)
             res.send(err);
 
