319a39f168a56a24e2a6267bbb2bc44ac55c7a61
cbstodd <cbstodd@gmail.com>
2016-10-01 18:15:58 -0400

    added EditCourseController for admins to edit

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,38 @@
 'use strict';
 
-var app = angular.module('noobLearn', ['ngRoute'])
-                 .config(function( $routeProvider, $locationProvider ) {
-                     $routeProvider
-                       .when('/', {
-                           templateUrl: 'views/partials/courses.ejs',
-                           controller:  'CourseController'
-                       })
-                       .when('/home', {
-                           templateUrl: 'views/partials/home.ejs',
-                           controller:  'HomeController'
-                       })
-                       .when('/courses', {
-                           templateUrl: 'views/partials/courses.ejs',
-                           controller:  'CourseController'
-                       })
-                       .when('/edit-course', {
-                           templateUrl: 'views/partials/edit-courses.ejs',
-                           controller:  'CourseController'
-                       })
-                       .when('/courses/:course_id', {
-                           templateUrl: 'views/courseDetails.ejs',
-                           controller:  'CourseDetailsController'
-                       })
-                       .when('/likes', {
-                           templateUrl: 'views/likes.ejs',
-                           controller:  'LikesController'
-                       })
-                       .otherwise({
-                           templateUrl: '/'
-                       });
-                     $locationProvider.html5Mode(true);
-                 });
+var app = angular.module('noobLearn', ['ngRoute']);
+
+app.config(function( $routeProvider, $locationProvider ) {
+    $routeProvider
+      .when('/', {
+          templateUrl: 'views/partials/courses.ejs',
+          controller:  'CourseController'
+      })
+      .when('/home', {
+          templateUrl: 'views/partials/home.ejs',
+          controller:  'HomeController'
+      })
+      .when('/courses', {
+          templateUrl: 'views/partials/courses.ejs',
+          controller:  'CourseController'
+      })
+      .when('/edit-course', {
+          templateUrl: 'views/partials/edit-courses.ejs',
+          controller:  'EditCourseController'
+      })
+      .when('/courses/:course_id', {
+          templateUrl: 'views/courseDetails.ejs',
+          controller:  'CourseDetailsController'
+      })
+      .when('/likes', {
+          templateUrl: 'views/likes.ejs',
+          controller:  'LikesController'
+      })
+      .otherwise({
+          templateUrl: '/'
+      });
+    $locationProvider.html5Mode(true);
+});
 
 
 
