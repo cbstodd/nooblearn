dae80febd810dc3024e7c06272893c056fa8240c
cbstodd <cbstodd@gmail.com>
2016-10-02 12:50:24 -0400

    refactored server.js to mongoose file

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ var express          = require('express'),
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var config = require('./server/config/config')[env];
-
-mongoose.connect(config.db);
-
 var app = express();
+
+var config = require('./server/config/config')[env];
 
 //pulls and envokes config route function in express.js
 require('./server/config/express')(app, config);
+require('./server/config/mongoose')(config);
 require('./server/config/routes')(app);
 
 // Start the server
